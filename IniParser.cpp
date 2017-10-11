25debaa3a34a7f2560e9fb949d0a2c89c4095032
Daniil Nizovkin <dan_nizovkin_kungur@mail.ru>
2017-10-11 22:59:58 +0300

    start of division

--- IniParser.cpp
+++ IniParser.cpp
@@ -2,53 +2,54 @@
 // Created by DNS on 17.09.2017.
 //
 #include "IniParser.h"
+
 using namespace std;
 
-void IniParser::Initialize(const char *filename_cstr)  throw (exc_io){
+void IniParser::Initialize(const char *filename_cstr)  throw(exc_io) {
     ifstream file(filename_cstr);
-    if(file.good()) {
-        string lineOfIniFile, sectionName, parameterName, parameterValue;
-        getline(file, lineOfIniFile);
-        int equalPosition, splittingPosition;
-        while (!file.eof()){
-            if(lineOfIniFile[0] == ';' || lineOfIniFile == "") {
-                getline(file,lineOfIniFile);
-                continue;
-            }
-            if (lineOfIniFile[0] == '[') {
-                sectionName = lineOfIniFile;
-                getline(file, lineOfIniFile);
-                while (lineOfIniFile[0] != '[' && !file.eof()) {
-                    if(lineOfIniFile[0] == ';' || lineOfIniFile == ""){
-                        getline(file,lineOfIniFile);
-                        continue;
-                    }
-                    if (lineOfIniFile[0] != ';') {
-                        lineOfIniFile.erase(remove(lineOfIniFile.begin(), lineOfIniFile.end(), ' '), lineOfIniFile.end());
-                        equalPosition = (int) lineOfIniFile.find('=');
-                        parameterName = lineOfIniFile.substr(0, (unsigned long long int) equalPosition);
-                        parameterValue = lineOfIniFile.substr((unsigned long long int) equalPosition + 1);
-                        splittingPosition = (int) parameterValue.find(';');
-                        if (splittingPosition != -1) {
-                            parameterValue = parameterValue.substr(0, (unsigned long long int) (splittingPosition));
-                        }
-
-                        dataStore[sectionName][parameterName] = parameterValue;
-                    }
+    if (!file.good()) {
+        throw exc_io("EXCEPTION: \"File not found!\"");
+    }
+    string lineOfIniFile, sectionName, parameterName, parameterValue;
+    getline(file, lineOfIniFile);
+    int equalPosition, splittingPosition;
+    while (!file.eof()) {
+        if (isLineComment(lineOfIniFile) || lineOfIniFile.empty()) {
+            getline(file, lineOfIniFile);
+            continue;
+        }
+        if (lineOfIniFile[0] == '[') {
+            sectionName = lineOfIniFile;
+            getline(file, lineOfIniFile);
+            while (lineOfIniFile[0] != '[' && !file.eof()) {
+                if (isLineComment(lineOfIniFile) || lineOfIniFile.empty()) {
                     getline(file, lineOfIniFile);
+                    continue;
                 }
+                if (lineOfIniFile[0] != ';') {
+                    lineOfIniFile.erase(remove(lineOfIniFile.begin(), lineOfIniFile.end(), ' '), lineOfIniFile.end());
+                    equalPosition = (int) lineOfIniFile.find('=');
+                    parameterName = lineOfIniFile.substr(0, (unsigned long long int) equalPosition);
+                    parameterValue = lineOfIniFile.substr((unsigned long long int) equalPosition + 1);
+                    splittingPosition = (int) parameterValue.find(';');
+                    if (splittingPosition != -1) {
+                        parameterValue = parameterValue.substr(0, (unsigned long long int) (splittingPosition));
+                    }
+
+                    dataStore[sectionName][parameterName] = parameterValue;
+                }
+                getline(file, lineOfIniFile);
             }
         }
-        equalPosition = (int) lineOfIniFile.find('=');
-        parameterName = lineOfIniFile.substr(0, (unsigned long long int) equalPosition - 1);
-        parameterValue = lineOfIniFile.substr((unsigned long long int) equalPosition + 2);
-        dataStore[sectionName][parameterName] = parameterValue;
     }
-    else throw exc_io("EXCEPTION: \"File not found!\"");
+    equalPosition = (int) lineOfIniFile.find('=');
+    parameterName = lineOfIniFile.substr(0, (unsigned long long int) equalPosition - 1);
+    parameterValue = lineOfIniFile.substr((unsigned long long int) equalPosition + 2);
+    dataStore[sectionName][parameterName] = parameterValue;
 }
 
-bool IniParser::IsHaveSection(const char *section_name) const throw(exc_ini_not_initied){
-    if(dataStore.empty()){
+bool IniParser::IsHaveSection(const char *section_name) const throw(exc_ini_not_initied) {
+    if (dataStore.empty()) {
         throw exc_ini_not_initied("EXCEPTION: \"File is not initialized\"");
     }
     map<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>, std::map<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>>>::const_iterator it;
@@ -61,36 +62,29 @@ bool IniParser::IsHaveSection(const char *section_name) const throw(exc_ini_not_
     return check;
 }
 
-bool IniParser::IsHaveParam(const char *section_name, const char *param_name) const throw(exc_ini_not_initied, exc_ini_not_found) {
-    if(dataStore.empty()){
+bool IniParser::IsHaveParam(const char *section_name,
+                            const char *param_name) const throw(exc_ini_not_initied, exc_ini_not_found) {
+    if (dataStore.empty()) {
         throw exc_ini_not_initied("EXCEPTION: \"File is not initialized\"");
     }
     bool checkParamExc = false;
     map<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>>::const_iterator it;
-    for (it = dataStore.at(section_name).begin(); it != dataStore.at(section_name).end() ; ++it) {
-        if(param_name == it->first){
+    for (it = dataStore.at(section_name).begin(); it != dataStore.at(section_name).end(); ++it) {
+        if (param_name == it->first) {
             checkParamExc = true;
         }
     }
-    if(!checkParamExc){
+    if (!checkParamExc) {
         throw exc_ini_not_found("EXCEPTION: \"Parameter not found!\"");
     }
     return checkParamExc;
 }
 
 
-void IniParser::print() const{
-    map<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>, std::map<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>>>::const_iterator it;
-    for (it = dataStore.begin(); it != dataStore.end(); ++it) {
-        cout << it->first << endl;
-        map<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>>::const_iterator it2;
-        for (it2 = dataStore.at(it->first).begin(); it2 != dataStore.at(it->first).end(); it2++) {
-            cout << it2->first << " = " << it2->second << endl;
-        }
-    }
+bool IniParser::isLineComment(std::string &lineOfIniFile) const{
+    return lineOfIniFile[0] == ';';
 }
 
-IniParser::~IniParser() {
 
-}
+IniParser::~IniParser() = default;
 
--- IniParser.h
+++ IniParser.h
@@ -20,27 +20,30 @@ private:
 public:
 
     IniParser() = default;
+
     ~IniParser();
 
-    void Initialize(const char* fileName) throw (exc_io);
+    void Initialize(const char *fileName) throw(exc_io);
 
-    bool IsHaveSection(const char* section_name) const  throw (exc_ini_not_initied);
+    bool IsHaveSection(const char *section_name) const throw(exc_ini_not_initied);
 
-    bool IsHaveParam(const char* section_name, const char* param_name) const throw (exc_ini_not_found, exc_ini_not_initied);
+    bool
+    IsHaveParam(const char *section_name, const char *param_name) const throw(exc_ini_not_found, exc_ini_not_initied);
 
     template<class T>
-    T GetValue(const char* section_name, const char* param_name) const throw (exc_ini_not_found, exc_ini_not_initied, exc_wrong_type){
-        if(dataStore.empty()){
+    T GetValue(const char *section_name,
+               const char *param_name) const throw(exc_ini_not_found, exc_ini_not_initied, exc_wrong_type) {
+        if (dataStore.empty()) {
             throw exc_ini_not_initied("EXCEPTION: \"File is not initialized\"");
         }
         bool check = false;
         map<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>>::const_iterator it;
-        for (it = dataStore.at(section_name).begin(); it != dataStore.at(section_name).end() ; ++it) {
-            if(param_name == it->first){
+        for (it = dataStore.at(section_name).begin(); it != dataStore.at(section_name).end(); ++it) {
+            if (param_name == it->first) {
                 check = true;
             }
         }
-        if(!check){
+        if (!check) {
             throw exc_ini_not_found("EXCEPTION: \"Parameter not found!\"");
         }
         string str = dataStore.at(section_name).at(param_name);
@@ -53,9 +56,8 @@ public:
         */
         return paramValue;
     }
-
-    void print() const;
+private:
+    bool isLineComment(std::string &lineOfIniFile) const;
 };
-
 
 #endif //LABA1_INIPARSER_H
